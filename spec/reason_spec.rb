d61e34ea1c97b9f93d6760e650f806d290135bfd
John Hernandez <john@writecode.ninja>
2021-01-23 14:34:39 -0600

    Add RSpec positive and negative test cases

--- spec/reason_spec.rb
+++ spec/reason_spec.rb
@@ -1,9 +1,18 @@
 #!/usr/bin/env ruby
 
-require_relative '../reason'
-
-let(:values) { Reason.new }
-let(:value) { values.select_value }
+require_relative '../lib/reason'
 
 RSpec.describe 'Reason' do
+  let(:values) do
+    values = Reason.new
+    values.select_value
+  end
+  describe '#select_value' do
+    it 'Should always return a hash' do
+      expect(values.class).to be(Hash)
+    end
+    it 'Should never return a nil result' do
+      expect(values).not_to be(nil)
+    end
+  end
 end
