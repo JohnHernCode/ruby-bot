5219b429fe6c9fe6931955fb037a9bbaed48e62b
John Hernandez <john@writecode.ninja>
2021-01-23 14:31:49 -0600

    Add image attr_reader and shortened select_value

--- lib/reason.rb
+++ lib/reason.rb
@@ -4,10 +4,16 @@ require 'json'
 require_relative 'bot'
 
 class Reason
-  @values = nil
+  attr_reader :img
 
+  @values = nil
   def initialize
     @values = chuck_json
+    @img = 'https://api.chucknorris.io/img/chucknorris_logo_coloured_small@2x.png'
+  end
+
+  def select_value
+    @values.slice('value')
   end
 
   def chuck_json
@@ -15,10 +21,5 @@ class Reason
     uri = URI(url)
     response = Net::HTTP.get(uri)
     JSON.parse(response)
-  end
-
-  def select_value
-    @values = @values.slice('value')
-    @values
   end
 end
