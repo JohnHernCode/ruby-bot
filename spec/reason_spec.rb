30e667470c1b52992e8de3e78e3c6c471a79c364
John Hernandez <john@writecode.ninja>
2021-01-23 14:59:27 -0600

    Add more test for the attr_reader

--- spec/reason_spec.rb
+++ spec/reason_spec.rb
@@ -7,9 +7,19 @@ RSpec.describe 'Reason' do
     values = Reason.new
     values.select_value
   end
+  let(:instance) { Reason.new }
+
   describe '#select_value' do
     it 'Should always return a hash' do
       expect(values.class).to be(Hash)
+    end
+    it 'Should never return a nil result' do
+      expect(values).not_to be(nil)
+    end
+  end
+  describe 'attr_reader:img' do
+    it 'Should return a string' do
+      expect(instance.img.class).to be(String)
     end
     it 'Should never return a nil result' do
       expect(values).not_to be(nil)
